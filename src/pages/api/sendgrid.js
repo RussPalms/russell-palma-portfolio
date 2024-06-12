22883bfe19d9a44b27ea6699de587254d8104453
Russell Brian Fulache Dugaduga Jale Palma <me@russellpalma.com>
2024-06-11 18:48:34 -0700

    ok

--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -1,60 +1,184 @@
-import sendgrid from "@sendgrid/mail";
+// import sendgrid from "@sendgrid/mail";
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+// sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+// async function sendEmail(req, res) {
+// 	try {
+// 		// console.log(''REQ.BODY', req.body);
+// 		const response = await sendgrid.send({
+// 			// to: "me@russellpalma.com",
+// 			// from: "me@russellpalma.com", // Website email address
+// 			// to: "russb.palms@gmail.com",
+// 			to: `${req.body.email}`,
+// 			// from: "russb.palms@gmail.com",
+// 			from: "russb.palms@gmail.com",
+// 			subject: `[Lead from website] ${req.body.subject}`,
+// 			// html: `<div>You've got mail!</div>`,
+// 			html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+//             <html lang="en">
+//             <head>
+//               <meta charset="utf-8">
+
+//               <title>The HTML5 Herald</title>
+//               <meta name="description" content="The HTML5 Herald">
+//               <meta name="author" content="SitePoint">
+//             <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+
+//               <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+//             </head>
+
+//             <body>
+//               <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
+//                     </div>
+//                     <div class="container" style="margin-left: 20px;margin-right: 20px;">
+//                     <h3>You've got new mail from ${req.body.fullname}, their email is: ✉️${req.body.email} </h3>
+//                     <div style="font-size: 16px;">
+//                     <p>Message:</p>
+//                     <p>${req.body.message}</p>
+//                     <br>
+//                     </div>
+//                     <img src="https://www.science4all.org/wp-content/uploads/2013/01/atom711.jpg" class="logo-image" style="height: 50px;width: 50px;border-radius: 5px;overflow: hidden;">
+//                     <p class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards<br>Russell Palma<br>Software Developer<br>+91 9587738861</p>
+//                     <div class="footer-links" style="display: flex;justify-content: center;align-items: center;">
+//                       <a href="https://github.com/RussPalms/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">GitHub</a>
+//                       <a href="https://instagram.com/rpalmpinoy/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Instagram</a>
+//                       <a href="https://linkedin.com/in/russell-palma-6b9700b9/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">LinkedIn</a>
+//                       <a href="https://twitter.com/rPalmPinoy/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Twitter</a>
+
+//                     </div>
+//                     </div>
+//             </body>
+//             </html>`,
+// 		});
+// 		console.log("response", response);
+// 	} catch (error) {
+// 		console.log(error);
+// 		return res
+// 			.status(error.statusCode || 500)
+// 			.json({ error: error.message });
+// 	}
+
+// 	return res.status(200).json({ error: "" });
+// }
+
+// export default sendEmail;
+
+import sgMail from "@sendgrid/mail";
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
-	try {
-		// console.log(''REQ.BODY', req.body);
-		await sendgrid.send({
-			to: "russb.palms@gmail.com",
-			from: "russb.palms@gmail.com", // Website email address
-			subject: `[Lead from website] ${req.body.subject}`,
-			// html: `<div>You've got mail!</div>`,
-			html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-            <html lang="en">
-            <head>
-              <meta charset="utf-8">
-            
-              <title>The HTML5 Herald</title>
-              <meta name="description" content="The HTML5 Herald">
-              <meta name="author" content="SitePoint">
-            <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-            
-              <link rel="stylesheet" href="css/styles.css?v=1.0">
-            
-            </head>
-            
-            <body>
-              <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
-                    </div>
-                    <div class="container" style="margin-left: 20px;margin-right: 20px;">
-                    <h3>You've got new mail from ${req.body.fullname}, their email is: ✉️${req.body.email} </h3>
-                    <div style="font-size: 16px;">
-                    <p>Message:</p>
-                    <p>${req.body.message}</p>
-                    <br>
-                    </div>
-                    <img src="https://www.science4all.org/wp-content/uploads/2013/01/atom711.jpg" class="logo-image" style="height: 50px;width: 50px;border-radius: 5px;overflow: hidden;">
-                    <p class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards<br>Russell Palma<br>Software Developer<br>+91 9587738861</p>
-                    <div class="footer-links" style="display: flex;justify-content: center;align-items: center;">
-                      <a href="https://github.com/RussPalms/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">GitHub</a>
-                      <a href="https://instagram.com/rpalmpinoy/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Instagram</a>
-                      <a href="https://linkedin.com/in/russell-palma-6b9700b9/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">LinkedIn</a>
-                      <a href="https://twitter.com/rPalmPinoy/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Twitter</a>
-                      
-                    </div>
-                    </div>
-            </body>
-            </html>`,
-		});
-	} catch (error) {
-		// console.log(error);
-		return res
-			.status(error.statusCode || 500)
-			.json({ error: error.message });
-	}
+	const { email, subject, message } = req.body;
 
-	return res.status(200).json({ error: "" });
+	// const emailContent = renderToStaticMarkup(
+	//     // `<SendTemplate fullname=${fullname} email=${email} message=${message} />`
+	//     SendTemplate({ fullname, email, message })
+	// );
+
+	const msg = {
+		to: email,
+		from: "connect@mail.driptrace.io", // Use the email address or domain you verified above
+		subject: subject,
+		text: message,
+		html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+	};
+
+	try {
+		await sgMail
+			.send(msg)
+			.then((response) => {
+				console.log("status code: ", response[0].statusCode);
+				console.log("response headers: ", response[0].headers);
+				if (response[0].statusCode === 202) {
+					console.log("Email sent successfully");
+					return res
+						.status(200)
+						.json({ message: "Email sent successfully" });
+				} else {
+					console.log("Error sending email:", response[0].headers);
+					return res
+						.status(500)
+						.json({ error: "Error sending email" });
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+
+		// const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 		Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+		// 	},
+		// 	body: JSON.stringify({
+		// 		personalizations: [
+		// 			{
+		// 				to: [
+		// 					{
+		// 						email: req.body.email,
+		// 					},
+		// 				],
+		// 				subject: `[Lead from website] ${req.body.subject}`,
+		// 			},
+		// 		],
+		// 		from: {
+		// 			email: "connect@mail.rpalm.net",
+		// 		},
+		// 		content: [
+		// 			{
+		// 				type: "text/html",
+		// 				value: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+		//       <html lang="en">
+		//       <head>
+		//         <meta charset="utf-8">
+
+		//         <title>The HTML5 Herald</title>
+		//         <meta name="description" content="The HTML5 Herald">
+		//         <meta name="author" content="SitePoint">
+		//       <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+
+		//         <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+		//       </head>
+
+		//       <body>
+		//         <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
+		//               </div>
+		//               <div class="container" style="margin-left: 20px;margin-right: 20px;">
+		//               <h3>You've got new mail from ${req.body.fullname}, their email is: ✉️${req.body.email} </h3>
+		//               <div style="font-size: 16px;">
+		//               <p>Message:</p>
+		//               <p>${req.body.message}</p>
+		//               <br>
+		//               </div>
+		//               <p class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards<br>Russell Palma<br>Software Developer<br>+91 9587738861</p>
+		//               <div class="footer-links" style="display: flex;justify-content: center;align-items: center;">
+		//                 <a href="https://github.com/RussPalms/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">GitHub</a>
+		//                 <a href="https://instagram.com/rpalmpinoy/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Instagram</a>
+		//                 <a href="https://linkedin.com/in/russell-palma-6b9700b9/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">LinkedIn</a>
+		//                 <a href="https://twitter.com/rPalmPinoy/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Twitter</a>
+
+		//               </div>
+		//               </div>
+		//       </body>
+		//       </html>`,
+		// 			},
+		// 		],
+		// 	}),
+		// });
+
+		// if (response.ok) {
+		// 	console.log("Email sent successfully");
+		// 	return res.status(200).json({ message: "Email sent successfully" });
+		// } else {
+		// 	console.log("Error sending email:", response.statusText);
+		// 	return res.status(500).json({ error: "Error sending email" });
+		// }
+	} catch (error) {
+		console.log("Error:", error);
+		return res.status(500).json({ error: "Internal server error" });
+	}
 }
 
 export default sendEmail;
